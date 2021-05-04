b42a8478038d93c8ef41a023ebb39b42a9aecd7f
Lu <luis.maltsis@mng.ch>
2021-05-04 12:18:33 +0200

    added add_points cheat method

--- bot/admin.py
+++ bot/admin.py
@@ -42,7 +42,6 @@ class CheatCog(commands.Cog):
     @commands.command()
     @commands.has_permissions(administrator = True)
     @commands.check(lib.checks.guild_exists_check)
-    @commands.check(lib.checks.user_exists_check)
     async def add_monster(self, ctx, user_name, monster_name, amount=1):
         user = lib.getters.get_user(user_name, ctx.guild.members)
         
@@ -59,6 +58,22 @@ class CheatCog(commands.Cog):
         for i in range(amount):
             db.Monster.create(monster['name'], 1, ctx.guild.id, owner_id)
         await ctx.message.channel.send(f'{user.mention} has been given {monster_name} (x{amount})')
+
+    @commands.command()
+    @commands.has_permissions(administrator = True)
+    @commands.check(lib.checks.guild_exists_check)
+    async def add_points(self, ctx, user_name, amount: int):
+        user = lib.getters.get_user(user_name, ctx.guild.members)
+        
+        if user is None or not await lib.checks.user_exists(user.id, ctx.guild.id):
+            await ctx.message.channel.send(f'User {user_name} not found')
+            return
+
+        user_db = db.User.get_by_member(ctx.guild.id, ctx.message.author.id)
+
+        db.User.set_score(ctx.message.author.id, ctx.guild.id, user_db.score + amount)
+        
+        await ctx.message.channel.send(f'{user.mention} score has been set from ``{user_db.score}`` to ``{user_db.score + amount}``')
 
 
 def setup(bot):
