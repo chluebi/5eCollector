3abe6684ceb7542958f55c2c8e119521f02945ed
Lu <luis.maltsis@mng.ch>
2021-06-19 21:05:11 +0200

    clarified underdark effect

--- lib/traits.py
+++ lib/traits.py
@@ -34,7 +34,7 @@ traits = {
                 'effects': [
                     {
                         'amount': 5,
-                        'text': 'At the start of an underdark\'s turn, if an ally has less hp then the underdark, the underdark consumes them, adding all of the ally\'s hp and stats (but not ac) to themselves.'
+                        'text': 'At the start of an underdark\'s turn, if the next ally attacker has less hp then the underdark, the underdark consumes them, adding all of the ally\'s hp and stats (but not ac) to themselves.'
                     }
                 ],
                 'note': ''
