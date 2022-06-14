fad93e1285e462fff05cd8fd2f563f2898531683
Lu <lu.maltsis@gmail.com>
2022-06-14 22:16:27 +0200

    fixed shapechanger error

--- bot/combat.py
+++ bot/combat.py
@@ -385,10 +385,11 @@ class Battle:
         if attacker_side.has_trait('shapechanger', amount=10):
             max_stat = 0
             for monster in attacker_side.alive_fighters() + target_side.alive_fighters():
-                max_stat = max(monster['stats'][self.stat])
+                max_stat = max(monster.mod(self.stat), max_stat)
             modifier = (max_stat, '👽')
         elif attacker_side.has_trait('shapechanger', amount=5):
-            modifier = (attacker.mod(self.stat), '👽')
+            if target.mod(self.stat) > attacker.mod(self.stat):
+                modifier = (attacker.mod(self.stat), '👽')
         
         base_attack_roll = random.randint(1, 20)
         #🎯
@@ -418,8 +419,8 @@ class Battle:
             crit = False
 
 
-        attack_roll = base_attack_roll[0] + attacker.mod(self.stat)
-        attack_text = ''
+        attack_roll = base_attack_roll[0] + modifier[0]
+        attack_text = modifier[1]
 
         if attacker_side.has_trait('gith', amount=9) and attacker.has_trait('gith'):
             if attacker.cr < target.cr:
